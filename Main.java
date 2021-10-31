b43e506314d5c7d81d04dd5932358d31f33802f3
Kestrel-150 <91588855+Kestrel-150@users.noreply.github.com>
2021-10-31 16:31:38 +0300

    ver 1.1

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        // какой то проект
+        // новый коммит
+        int a = 2;
+        System.out.println("fixxx");
+    }
+}
